9decc18962a15add6b718bc3071dbbf428469dfe
shinsingchul <scshin@mrpic.co.kr>
2026-09-23 10:33:37 +0900

    app 수정

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->statefulApi();  // Sanctum Bearer 토큰 인증
         $middleware->validateCsrfTokens(except: [
             'nhis/fax-callback',
